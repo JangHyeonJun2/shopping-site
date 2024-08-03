0eac2c7e7389134ce8b1a034b0d8af21a9512e45
JangHyeonJun2 <epfvkdlxj@naver.com>
2024-08-03 23:14:15 +0900

    init: 초기 프로젝트 생성

---
+++ src/main/java/com/shoppingsite/shopppingsite/ShopppingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingsite.shopppingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopppingSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopppingSiteApplication.class, args);
+    }
+
+}
